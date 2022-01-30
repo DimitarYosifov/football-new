89d6504ba49d00efc90ce4ffe8586f1cd802a821
DimitarYosifov <yosifov1985@gmail.com>
2022-01-30 11:48:36 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { Manager } from './Manager';
+import { LoaderScene } from './scenes/LoaderScene';
+
+Manager.initialize(540, 960, 0x6495ed);
+
+// We no longer need to tell the scene the size because we can ask Manager!
+const loady: LoaderScene = new LoaderScene();
+Manager.changeScene(loady);
