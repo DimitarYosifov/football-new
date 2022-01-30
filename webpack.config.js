298ab2209aa0f6ebd3a836f79607533cdbde54e9
DimitarYosifov <yosifov1985@gmail.com>
2022-01-30 12:23:36 +0200

    make it better

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = (env, argv) => {
                 },
                 progress: true,
             },
-            port: 3000, host: '0.0.0.0'
+            port: 8080, host: '0.0.0.0'
         },
 
         // Web games are bigger than pages, disable the warnings that our game is too big.
